141ef7bdfe18cfde17f5cd9c1a0cef25fd6db039
Vich64 <callmevichika@gmail.com>
2025-08-14 14:04:40 +0700

    UpdateFolder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomePage from './component/homePage.jsx';
+import HomePage from './components/homePage.jsx';
 
 function App() {
   return (
