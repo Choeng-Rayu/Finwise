0bb474586c087baa61ef929f6549b88b56003907
choeng-rayu <choengrayu307@gmail.com>
2025-08-14 15:15:49 +0700

    delete s in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomePage from './components/homePage.jsx';
+import HomePage from './component/homePage.jsx';
 
 function App() {
   return (
